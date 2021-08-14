d25b581f1dc1f814d52be59e193e6d7fdc9e26c6
kimtaeyoung0321 <kty986433@gmail.com>
2021-08-14 15:11:58 +0900

    2nd update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 
 const PORT = 4000;
+
 const PORT = express();
 
-const handleListening = () => console.log('Server listening on port http://localhost:${PORT}');
+const homehandleHome = () => console.log("Somebody is trying to go home.");
+app.get("/", homehandleHome);
+
+const handleListening = () => 
+    console.log('Server listening on port http://localhost:${PORT}');
 
 app.listen(PORT, handleListening);
